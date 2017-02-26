64ae135c65e93a01cea04ec5ce6aa3d65919bc13
Vincent Smith <VinceS22@gmail.com>
2017-02-26 15:35:55 -0500

    Converting tabs to spaces for sanity sake.

--- main.rb
+++ main.rb
@@ -1,40 +1,40 @@
 # I'm completely new to ruby, be gentle!
 class QuizGenerator
-	@@naturalNotes = ["A","B","C","D","E","F","G"]
-	@@noteSuffixes = ["#", "b", ""]
+  @@naturalNotes = ["A","B","C","D","E","F","G"]
+  @@noteSuffixes = ["#", "b", ""]
 
-	def self.GenerateQuiz (amountOfQuestionsParam=-1, includeIntervals=true)
+  def self.GenerateQuiz (amountOfQuestionsParam=-1, includeIntervals=true)
 
-		amountOfQuestions = amountOfQuestionsParam
-		while amountOfQuestions == -1
-			puts "Please enter in the amount of questions that you desire"
-			amountOfQuestions = Integer(gets) rescue -1 
-		end
-		
-		(1..amountOfQuestions).each do |currentQuestionNumber|
-			firstNote = GenerateRandomNote()
-			print currentQuestionNumber.to_s + ") What interval is " + firstNote + " up to " 
-			preferredSuffix = ""
-			if @@noteSuffixes.include?(firstNote[-1,1])
-				preferredSuffix = firstNote[-1,1]
-			end
+    amountOfQuestions = amountOfQuestionsParam
+    while amountOfQuestions == -1
+      puts "Please enter in the amount of questions that you desire"
+      amountOfQuestions = Integer(gets) rescue -1 
+    end
+    
+    (1..amountOfQuestions).each do |currentQuestionNumber|
+      firstNote = GenerateRandomNote()
+      print currentQuestionNumber.to_s + ") What interval is " + firstNote + " up to " 
+      preferredSuffix = ""
+      if @@noteSuffixes.include?(firstNote[-1,1])
+        preferredSuffix = firstNote[-1,1]
+      end
 
-			puts GenerateRandomNote(preferredSuffix) + "?"
-		end
+      puts GenerateRandomNote(preferredSuffix) + "?"
+    end
 
-	end
+  end
 
-	def self.GenerateRandomNote (preferredSuffix="")
-		
-		selectedNote = @@naturalNotes[rand(@@naturalNotes.length)]
-		eligibleSuffixes = @@noteSuffixes
-		if selectedNote == "B" || selectedNote == "E" || preferredSuffix == "b"
-			eligibleSuffixes = eligibleSuffixes - ["#"]
-		elsif selectedNote == "C" || selectedNote == "F" || preferredSuffix == "#"
-			eligibleSuffixes = eligibleSuffixes - ["b"]
-		end
-		return selectedNote + @@noteSuffixes[rand(@@noteSuffixes.length)]
-	end
+  def self.GenerateRandomNote (preferredSuffix="")
+    
+    selectedNote = @@naturalNotes[rand(@@naturalNotes.length)]
+    eligibleSuffixes = @@noteSuffixes
+    if selectedNote == "B" || selectedNote == "E" || preferredSuffix == "b"
+      eligibleSuffixes = eligibleSuffixes - ["#"]
+    elsif selectedNote == "C" || selectedNote == "F" || preferredSuffix == "#"
+      eligibleSuffixes = eligibleSuffixes - ["b"]
+    end
+    return selectedNote + @@noteSuffixes[rand(@@noteSuffixes.length)]
+  end
 end
 
 QuizGenerator.GenerateQuiz()
