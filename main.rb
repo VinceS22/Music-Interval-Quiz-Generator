3cbf44f74152287575dcded955c2105a4d52ce6b
Vincent Smith <VinceS22@gmail.com>
2017-02-27 20:48:21 -0500

    Conforming to Ruby style guide.

--- main.rb
+++ main.rb
@@ -1,75 +1,74 @@
-require 'pry'
 # I'm completely new to ruby, be gentle!
 class QuizGenerator
-  @@naturalNotes = ["A","B","C","D","E","F","G"]
-  @@noteSuffixes = ["#", "b", ""]
-
-  def self.GenerateQuiz (amountOfQuestionsParam=-1, answerSpaceLines=-1, includeIntervals=true, preventDuplicates=true)
-    
-    questionsHash = Hash[@@naturalNotes.product(@@noteSuffixes).map {|note| [note.join(""), []]}]
+  def self.generate_quiz (amount_of_q=-1, answer_space_lines=-1, prevent_duplicates=true)
+    natural_notes = %w(A B C D E F G)
+    note_suffixes = ['#', 'b', '']
+    questions_hash = Hash[natural_notes.product(note_suffixes).map {|note| [note.join(""), []]}]
     # Removing the irrelevant notes
-    questionsHash.delete("Cb")
-    questionsHash.delete("B#")
-    questionsHash.delete("E#")
-    questionsHash.delete("Fb")
+    questions_hash.delete('Cb')
+    questions_hash.delete('B#')
+    questions_hash.delete('E#')
+    questions_hash.delete('Fb')
 
-    amountOfQuestions = amountOfQuestionsParam
-    while amountOfQuestions == -1
-      puts "Please enter in the amount of questions that you desire:"
-      amountOfQuestions = Integer(gets) rescue -1 
+    amount_of_questions = amount_of_q
+    while amount_of_questions == -1
+      puts 'Please enter in the amount of questions that you desire:'
+      amount_of_questions = Integer(gets) rescue -1
     end
 
-    while answerSpaceLines == -1
+    while answer_space_lines == -1
       puts "Enter a number for the amount of lines you want for answer space, if you don't want any, select 0:"
-      answerSpaceLines = Integer(gets) rescue -1 
+      answer_space_lines = Integer(gets) rescue -1
     end
 
-    (1..amountOfQuestions).each do |currentQuestionNumber|
-      firstNote = GenerateRandomNote()
-      
+    (1..amount_of_questions).each do |currentQuestionNumber|
+      first_note = generate_random_note
+
       # We don't want to mix flats and sharps, so we need to get the preferred suffix
-      preferredSuffix = ""
-      if @@noteSuffixes.include?(firstNote[-1,1])
-        preferredSuffix = firstNote[-1,1]
+      preferred_suffix = ''
+      if note_suffixes.include?(first_note[-1,1])
+        preferred_suffix = first_note[-1,1]
       end
 
-      secondNote = GenerateRandomNote(preferredSuffix)
+      second_note = generate_random_note(preferred_suffix)
 
       # We don't want duplicate questions, so retry.
-      if preventDuplicates 
-        if questionsHash[firstNote].include?(secondNote)
+      if prevent_duplicates
+        if questions_hash[first_note].include?(second_note)
           redo
         else
-          questionsHash[firstNote].push(secondNote)
+          questions_hash[first_note].push(second_note)
         end
       end
 
-      PrintQuestion(currentQuestionNumber, firstNote, secondNote, answerSpaceLines)
+      print_question(currentQuestionNumber, first_note, second_note, answer_space_lines)
     end
 
   end
 
-  def self.GenerateRandomNote (preferredSuffix="")
-    
-    selectedNote = @@naturalNotes[rand(@@naturalNotes.length)]
-    eligibleSuffixes = @@noteSuffixes
+  def self.generate_random_note (preferred_suffix='')
 
-    if selectedNote == "B" || selectedNote == "E" || preferredSuffix == "b"
-      eligibleSuffixes = ["b", ""]
-    elsif selectedNote == "C" || selectedNote == "F" || preferredSuffix == "#"
-      eligibleSuffixes = ["#", ""]
+    natural_notes = %w(A B C D E F G)
+    note_suffixes = ['#', 'b', '']
+    selected_note = natural_notes[rand(natural_notes.length)]
+    eligible_suffixes = note_suffixes
+
+    if selected_note == 'B' || selected_note == 'E' || preferred_suffix == 'b'
+      eligible_suffixes = ['b', '']
+    elsif selected_note == 'C' || selected_note == 'F' || preferred_suffix == '#'
+      eligible_suffixes = ['#', '']
     end
 
-    return selectedNote + eligibleSuffixes[rand(eligibleSuffixes.length)]
+    selected_note + eligible_suffixes[rand(eligible_suffixes.length)]
   end
 
-  def self.PrintQuestion (currentQuestionNumber, note1, note2, answerSpaceLines)
-    puts currentQuestionNumber.to_s + ") What interval is " + note1 + " up to " + note2 + "?"
+  def self.print_question (current_question_num, note1, note2, answer_space_lines)
+    puts current_question_num.to_s + ') What interval is ' + note1 + ' up to ' + note2 + '?'
 
-    (1..answerSpaceLines).each do |x|
-      puts ""
+    (1..answer_space_lines).each do
+      puts ''
     end
   end
 end
 
-QuizGenerator.GenerateQuiz()
+QuizGenerator.generate_quiz(30, 3, true)
